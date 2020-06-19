36f9bc3f8278f2b22ce93c231e98cd2633a61d05
Becca Burke <reburke286@gmail.com>
2020-06-18 21:13:05 -0700

    updated PORT code in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var nodemailer = require("nodemailer");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
