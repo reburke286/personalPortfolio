5fcca6aabba597b431ddd4c5f9cc0cbfabcf3a2d
Becca Burke <reburke286@gmail.com>
2020-06-18 18:08:16 -0700

    added and enabled nodemailer to contact form

--- public/script/index.js
+++ public/script/index.js
@@ -2,6 +2,15 @@ $(document).ready(function () {
   $(".modal").modal();
 
   $(".submitBtn").click(function () {
+    event.preventDefault();
+    const msg = {
+      name: $("#name").val().trim(),
+      email: $("#email").val().trim(),
+      message: $("#message").val().trim(),
+    };
+    $.post("/msg", msg).then(function (req, res) {
+      console.log("msg", msg);
+    });
     $(".modal").modal("close");
   });
 
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var nodemailer = require("nodemailer");
+// var bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
@@ -11,12 +13,16 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
+
+//Messages
+const messages = [];
 
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// Get Routes
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/html/index.html"));
 });
@@ -24,6 +30,60 @@ app.get("/", function (req, res) {
 app.get("/portfolio", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/html/portfolio.html"));
 });
+
+app.get("msg", function (req, res) {
+  return res.json(messages);
+});
+
+// Post Routes
+app.post("/msg", (req, res) => {
+  const output = `
+  <p>You have a new message</p>
+  <h3>Contact Details</h3>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h3>Message</h3>
+  <p> ${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "reburke286",
+      pass: "$croogeMcDuck3",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // send mail with defined transport object
+  let info = transporter.sendMail({
+    from: '"Portfolio Contact" <reburke286@gmail.com>',
+    to: "rebeccadburke@gmail.com",
+    subject: "Contact from Portfolio",
+    html: output,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  // main().catch(console.error);
+  const newMsg = req.body;
+  messages.push(newMsg);
+  console.log(req.body);
+  res.json(newMsg);
+});
+
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
